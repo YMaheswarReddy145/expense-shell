b78993a05a5a45d71ffaa6af5c28ddb14da73129
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-11-03 00:53:01 +0530

    Expense Project

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,29 @@
-echo -e "\e[36m Installing nginx \e[0m"
-dnf install nginx -y
+log_file="/temp/expense.log"
+color="\e[33m"
 
-echo -e "\e[36m Copying Expense Configuration \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e "\e[36m Removing the old nginx content \e[0m"
-rm -rf /usr/share/nginx/html/*
+echo -e "${color} Installing nginx \e[0m"
+dnf install nginx -y &>>$log_file
+echo $?
 
-echo -e "\e[36m Download Frontend Application Code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "${color} Copying Expense Configuration \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
 
-echo -e "\e[36m Extract Download Application Content \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "${color} Removing the old nginx content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
-echo -e "\e[36m Starting nginx services \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+echo -e "${color} Download Frontend Application Code \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
+
+echo -e "${color} Extract Download Application Content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+echo $?
+
+echo -e "${color} Starting nginx services \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+echo $?
