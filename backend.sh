a9fad461f079b04a2947e209be68658d68998811
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-11-03 19:14:30 +0530

    Expense Project

--- backend.sh
+++ backend.sh
@@ -33,13 +33,16 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi 
 
-echo -e "${color} Add Application User \e[0m"
-useradd expense &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+id expene &>>$log_file
+if [ $? -ne 0 ]; then   # we are adding this to evaluate if the user expense  is present or not.
+  echo -e "${color} Add Application User \e[0m"
+  useradd expense &>>$log_file
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+fi
 
 echo -e "${color} Create Application directory \e[0m"
 mkdir /app &>>$log_file
