de09517d0701bfeebd20d81171785aede357d401
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-11-06 19:02:16 +0530

    Expense Project

--- backend.sh
+++ backend.sh
@@ -10,47 +10,38 @@ MYSQL_ROOT_PASSWORD=$1  # with the help of this now we don't need to hard code t
                         # directly provide the password in the script after the file name.
                            # sudo bash backend.sh Expenasepp@1
 
-echo -e "${color} Disable NodeJS default Version \e[0m"
-dnf module disable nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
-
-echo -e "${color} Enable NodeJS 18 Version \e[0m"
-dnf module enable nodejs:18 -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
-
-echo -e "${color} Install NodeJS \e[0m"
-dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
-
-echo -e "${color} Copy Backend Service File \e[0m"
-cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
-
-id expense &>>$log_file
-if [ $? -ne 0 ]; then   # we are adding this to evaluate if the user expense  is present or not.
-  echo -e "${color} Add Application User \e[0m"
-  useradd expense &>>$log_file
+# previously at each step we are providing the below condition to check the status but now with the help of functions
+# we can declare the condition in the functions and we can call the function directly where ever we want to check the status.
+status_check()
+{
   if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
   else
     echo -e "\e[31m FAILURE \e[0m"
   fi
+}
+
+echo -e "${color} Disable NodeJS default Version \e[0m"
+dnf module disable nodejs -y &>>$log_file
+status_check()
+
+echo -e "${color} Enable NodeJS 18 Version \e[0m"
+dnf module enable nodejs:18 -y &>>$log_file
+status_check()
+
+echo -e "${color} Install NodeJS \e[0m"
+dnf install nodejs -y &>>$log_file
+status_check()
+
+echo -e "${color} Copy Backend Service File \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
+status_check()
+
+id expense &>>$log_file
+if [ $? -ne 0 ]; then   # we are adding this to evaluate if the user expense  is present or not.
+  echo -e "${color} Add Application User \e[0m"
+  useradd expense &>>$log_file
+  status_check()
 fi
 
 # sudo mkdir -p /app to avoid the problem in the first place here -p is an option its ignore if there is an directory
@@ -59,68 +50,36 @@ fi
 if [ ! -d /app ]; then  # here -d represent that the directory /app is exist
   echo -e "${color} Create Application directory \e[0m"
   mkdir /app &>>$log_file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
+  status_check()
 fi
 
 echo -e "${color} Delete old  Application Content \e[0m"
 rm -rf /app/* &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Download Application Content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Extract Application Content \e[0m"
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Download NodeJS Dependencies \e[0m"
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Install MYSQL client to load the Schema \e[0m"
 dnf install mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Load Schema \e[0m"
 mysql -h mysql-dev.maheswary.online -uroot -p${MYSQL_ROOT_PASSWORD} < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
 
 echo -e "${color} Start Backend Services \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi 
+status_check()
